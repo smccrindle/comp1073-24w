cf07083e45134a5fd01fd6dee7617f137d195cd3
Scott McCrindle <scott@mccrindle.me>
2024-02-14 11:52:26 -0500

    Completed gallery.js and added the complete lab 1

--- lesson-6/js/gallery.js
+++ lesson-6/js/gallery.js
@@ -38,20 +38,5 @@ thumbBar.addEventListener("click", function(event) {
 		displayImage(imgSrc);
 	}	
 });
-		// Lab 6 STEP G: Call the clearWayfinding() function that you built below - and enjoy the result!
-
-		// Lab 6 STEP A: Inside the thumbBar.onclick event handler function, and also inside the if statement, change the event.target CSS outline property to "5px solid red"
-
-		// Lab 6 STEP B: Next, change the event.target CSS position property to "relative"
-
-		// Lab 6 STEP C: And then set the CSS z-index property to "10" so that the thumbnail clicked is on top of all the others
-
-
-// Lab 6 STEP D: Initialize and declare a var called 'thumbImgs' using the querySelectorAll method to grab all the IMG elements inside the .thumb-bar
-
-
-// Lab 6 STEP E: Build a function called 'clearWayfinding()' that loops through the thumbImgs array with a FOR loop
-
-		// Lab 6 STEP F: For each thumbImgs IMG element, set the CSS outline-width property to "0", and the z-index property also to "0"
 
 // This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Image_gallery and https://davidwalsh.name/event-delegate
