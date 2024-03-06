b840a03a83ada8833ec1e9dd476ee35486e605d3
Scott McCrindle <scott@mccrindle.me>
2024-03-06 10:55:07 -0500

    Completed OOP part of the lesson

--- lesson-8/js/oop.js
+++ lesson-8/js/oop.js
@@ -6,24 +6,40 @@ function createAlbum(name) {
     obj.name = name;
     obj.describe = function() {
         alert(`This album is called ${this.name}.`);
-    }
+    };
+    return obj;
 }
 
 /* STEP 1b: Use the console to create a new vehicle object, and then invoke the function represented using .description() */
 
 
 /* STEP 2a: In order to be a bit more concise, JavaScript allows us to use constructor functions - rewrite the above function, without returning anything. Capitalize the name of the function. */
-
+// function Album(name) {
+//     this.name = name;
+//     this.describe = function() {
+//         alert(`This album is called ${this.name}.`);
+//     };
+// }
 
 /* STEP 2b: Use the console to create a couple of different albums, using the 'new' keyword, and again invoking the .describe() method for each one */
 
 
-/* STEP 3a: Build the complete constructor for the object Vehicle (comment out the above functions first). Include full vehicle name (manufacturer and model), year, type, colour, and description (function). */
-
-
+/* STEP 3a: Build the complete constructor for the object Album (comment out the above functions first). Include full vehicle name (manufacturer and model), year, type, colour, and description (function). */
+function Album(name, artist, year, numTracks) {
+    this.name = name;
+    this.artist = artist;
+    this.year = year;
+    this.numTracks = numTracks;
+    this.describe = function() {
+        //alert(`The album ${this.name} by ${this.artist} was released in ${this.year}, featuring ${this.numTracks} song tracks.`);
+        let description = `The album ${this.name} by ${this.artist} was released in ${this.year}, featuring ${this.numTracks} song tracks.`;
+        return description;
+    }
+}
 
 /* STEP 3b: Instantiate a new Album (or 2) based on the above constructor */
-
+const album2 = new Album("Disintegration", "The Cure", 1989, 12);
+const album3 = new Album("Substance", "New Order", 1987, 24);
 
 /* STEP 3c: Attempt to access the various properties of album2 or album3 using the console. */
 // album2['name']
@@ -37,7 +53,7 @@ function createAlbum(name) {
 /* STEP 4a: Modify the describe method of the above Album constructor so that it returns a string. */
 
 /* STEP 4b: Capture the description of one of the album objects and set it as the text for the paragraph on the page (see line 1 of this script). */
-
+output.textContent = album3.describe();
 
 // That's it! Now on to the Lab...
 
